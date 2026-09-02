a3bf73062eecafe8318fd9949f3b787cc3d13c8c
a-grasso <antonino.grasso.it@gmail.com>
2026-09-02 18:36:37 +0200

    Bump the version to 0.2.0

--- main.swift
+++ main.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 // Single source of truth for the shipped version: build.sh reads it out of
 // here for Info.plist, and the release workflow refuses a tag that disagrees.
-let version = "0.1.2"
+let version = "0.2.0"
 
 // MARK: - config
 
